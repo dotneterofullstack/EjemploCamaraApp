13d84544f9037a931b0918623229084eb820d484
MacUser <omar.garcia.developer@hotmail.com>
2017-09-01 18:27:08 -0500

    Agregar Funcionalidad de camara

--- EjemploCmaraApp/EjemploCmaraAppPage.xaml.cs
+++ EjemploCmaraApp/EjemploCmaraAppPage.xaml.cs
@@ -7,6 +7,21 @@ namespace EjemploCmaraApp
         public EjemploCmaraAppPage()
         {
             InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        async void CameraButton_Clicked(object sender, System.EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(
+                new Plugin.Media.Abstractions.StoreCameraMediaOptions() {});
+
+            if (photo != null)
+            {
+                PhotoImage.Source = ImageSource.FromStream(
+                    () => photo.GetStream()
+                );
+            }
         }
     }
 }
